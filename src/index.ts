8cda6c78b5410890b4b17720a407c2c5b25cf36c
ygr4789 <yongdori@snu.ac.kr>
2023-05-05 19:33:13 +0900

    controllers, ground-collision, distance-constraint(demo), volume-constraint(demo)

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
-const setcolor = 0xbbbbbb;
+const setcolor = 0x000000;
+// const setcolor = 0xbbbbbb;
 scene.background = new THREE.Color(setcolor);
 
 const renderer = new THREE.WebGLRenderer({
@@ -12,7 +13,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000.0);
-camera.position.set(40, 40, 45);
+camera.position.set(1, 1, 1);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.listenToKeyEvents(window);
@@ -75,7 +76,7 @@ async function loadBunny() {
         });
 }
 
-// ===================== MAIN =====================
+// ===================== CLASS =====================
 
 class SoftBodyObject {
     init_positions: Array<number>;
@@ -88,14 +89,20 @@ class SoftBodyObject {
     mesh: THREE.Mesh;
     edge_geometry: THREE.BufferGeometry;
     edges: THREE.LineSegments;
+
+    tet_constrains: Array<Array<number>>;
+    init_tet_volumes: Array<number>;
+    edge_constrains: Array<Array<number>>;
+    init_edge_lengths: Array<number>;
+
     constructor(file: parsedData, _scene: THREE.Scene) {
         // this.init_positions
         this.init_positions = file.verts;
         this.positions = [];
         this.velocities = [];
 
-        for (let i = 0; i < this.init_positions.length / 3; i++) {
-            this.positions.push(new THREE.Vector3(this.init_positions[3 * i], this.init_positions[3 * i + 1], this.init_positions[3 * i + 2]));
+        for (let i = 0; i < this.init_positions.length; i += 3) {
+            this.positions.push(new THREE.Vector3(...this.init_positions.slice(i, i + 3)));
             this.velocities.push(new THREE.Vector3(0, 0, 0));
         }
 
@@ -117,6 +124,27 @@ class SoftBodyObject {
 
         this.edges = new THREE.LineSegments(this.edge_geometry, new THREE.LineBasicMaterial({ color: 0xffffff }));
         _scene.add(this.edges);
+
+        // ===================== GENERATE CONSTRAINS =====================
+
+        this.tet_constrains = [];
+        this.init_tet_volumes = [];
+        for (let i = 0; i < file.tetIds.length; i += 4) {
+            this.tet_constrains.push([...file.tetIds.slice(i, i + 4)]);
+            const [x0, x1, x2, x3] = [...this.tet_constrains[i / 4]].map((tetId) => this.positions[tetId]);
+            const x01 = new THREE.Vector3().subVectors(x1, x0);
+            const x02 = new THREE.Vector3().subVectors(x2, x0);
+            const x03 = new THREE.Vector3().subVectors(x3, x0);
+            this.init_tet_volumes.push(new THREE.Vector3().crossVectors(x01, x02).dot(x03) / 6);
+        }
+        
+        this.edge_constrains = [];
+        this.init_edge_lengths = [];
+        for (let i = 0; i < file.tetEdgeIds.length; i += 2) {
+            this.edge_constrains.push([...file.tetEdgeIds.slice(i, i + 2)]);
+            const [x0, x1] = [...this.edge_constrains[i / 2]].map((tetId) => this.positions[tetId]);
+            this.init_edge_lengths.push(x0.distanceTo(x1));
+        }
     }
 
     renderUpdate() {
@@ -135,14 +163,59 @@ class SoftBodyObject {
         this.edges.geometry.computeBoundingSphere();
     }
 
-    update() {
-        // Dummy Velocity (Going Up)
-        for (let i = 0; i < this.positions.length; i++) this.velocities[i] = new THREE.Vector3(0.0, 0.01, 0.0);
-
-        // Dummy Position Update
+    update(dt: number) {
+        const gravity = -0.1;
+        
+        let prev_positions = this.positions.map((v) => v.clone())
         for (let i = 0; i < this.positions.length; i++) {
-            this.positions[i].add(this.velocities[i]);
+            this.velocities[i].add(new THREE.Vector3(0, gravity * dt, 0));
+            this.positions[i].add(this.velocities[i].clone().multiplyScalar(dt));
+            
         }
+        
+        for (let i = 0; i < this.positions.length; i++) {
+            if (this.positions[i].y < 0.0) {
+                this.positions[i].setY(0.0);
+            }
+        }
+        
+        for (let i = 0; i < this.tet_constrains.length; i++) {
+            const [x0, x1, x2, x3] = [...this.tet_constrains[i]].map((tetId) => this.positions[tetId]);
+            const x01 = new THREE.Vector3().subVectors(x1, x0);
+            const x02 = new THREE.Vector3().subVectors(x2, x0);
+            const x03 = new THREE.Vector3().subVectors(x3, x0);
+            const x12 = new THREE.Vector3().subVectors(x1, x2);
+            const x13 = new THREE.Vector3().subVectors(x1, x3);
+            const volume = new THREE.Vector3().crossVectors(x01, x02).dot(x03) / 6;
+            const init_volume = this.init_tet_volumes[i];
+            const grad_x0_c = new THREE.Vector3().crossVectors(x13, x12);
+            const grad_x1_c = new THREE.Vector3().crossVectors(x02, x03);
+            const grad_x2_c = new THREE.Vector3().crossVectors(x03, x01);
+            const grad_x3_c = new THREE.Vector3().crossVectors(x01, x02);
+            const denom = [grad_x0_c, grad_x1_c, grad_x2_c, grad_x3_c].reduce((prev, cur) => {
+                return prev + (cur.length() ** 2);
+            }, 0)
+            const lambda = -6 * (volume - init_volume) / denom;
+            x0.add(grad_x0_c.multiplyScalar(lambda));
+            x1.add(grad_x1_c.multiplyScalar(lambda));
+            x2.add(grad_x2_c.multiplyScalar(lambda));
+            x3.add(grad_x3_c.multiplyScalar(lambda));
+        }
+        
+        for (let i = 0; i < this.edge_constrains.length; i++) {
+            const [x0, x1] = [...this.edge_constrains[i]].map((edgeId) => this.positions[edgeId]);
+            const x01 = new THREE.Vector3().subVectors(x1, x0);
+            const l = x01.length();
+            const l0 = this.init_edge_lengths[i];
+            x01.normalize();
+            x0.add(x01.clone().multiplyScalar(0.5 * (l - l0)));
+            x1.add(x01.clone().multiplyScalar(-0.5 * (l - l0)));
+        }
+        
+        for (let i = 0; i < this.positions.length; i++) {
+            this.velocities[i] = new THREE.Vector3().subVectors(this.positions[i], prev_positions[i]).multiplyScalar(1.0 / dt);
+        }
+        
         this.renderUpdate();
     }
 
@@ -160,7 +233,11 @@ class SoftBodyObject {
     }
 }
 
+// ===================== MAIN =====================
+
 let bunny: SoftBodyObject;
+let isPlay = false;
+let debugInfo: any;
 
 function main() {
     let prevTime = 0;
@@ -168,18 +245,38 @@ function main() {
 
     function animate(timestamp: number) {
         let timediff = (timestamp - prevTime) / 1000;
-        if (timestamp > 5000) return;
-        updateStates(timediff);
+        if(isPlay) updateStates(timediff);
         renderer.render(scene, camera);
         prevTime = timestamp;
     }
     function updateStates(dt: number) {
-        bunny.update();
+        bunny.update(dt);
+    }
+}
+
+
+function init_controllers() {
+    const controller = document.querySelector('#controller');
+    
+    const debugButton = document.createElement('button')
+    debugButton.onclick = test;
+    debugButton.innerText = 'Debug'
+    controller?.appendChild(debugButton);
+    
+    const runButton = document.createElement('button')
+    runButton.onclick = () => {isPlay = !isPlay};
+    runButton.innerText = 'Run'
+    controller?.appendChild(runButton);
+    
+    function test() {
+        console.log(debugInfo);
     }
 }
 
 window.onload = async () => {
     await loadBunny();
     bunny = new SoftBodyObject(bunnyData, scene);
+    init_controllers();
     main();
+    // test();
 };
